15bc68c6d7a83623845a3e9e683c27fb5c8ee1be
chandu <moulic726@gmail.com>
2026-05-20 19:04:00 +0530

    First commit

---
+++ basics/script.js
@@ -0,0 +1,16 @@
+alert("hello world from alert")
+
+console.log("hello world from console");
+
+document.write("hello world from document.write");
+
+
+
+function func(a,b){
+    return a + b;
+}
+console.log(func(10,20));
+
+let d=(a,b)=>{
+    return a + b
+};
